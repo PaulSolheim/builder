c4acaa575fc087b2d002856b73a4a20a71235d11
Pål Grønstad Solheim <paul.solheim@gmail.com>
2021-03-09 17:43:08 +0100

    Kraftsystem
    Kraftkilder, kraftmottakere og kraftledninger.

--- Autoload/Events.gd
+++ Autoload/Events.gd
@@ -1,5 +1,7 @@
 extends Node
 
 signal entity_placed(entity, cellv)
-
 signal entity_removed(entity, cellv)
+
+## Signal emitted when the simulation triggers the systems for updates.
+signal systems_ticked(delta)
